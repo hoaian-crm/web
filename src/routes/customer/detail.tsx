ef15268e884427a7f1a2cc4259eb00cdfedb4f53
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-05 22:43:28 +0700

    Add customer detail

--- src/routes/customer/detail.tsx
+++ src/routes/customer/detail.tsx
@@ -1,6 +1,71 @@
-import React from 'react';
+import { MailFilled, MailOutlined, MailTwoTone, ManOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Card, Col, DatePicker, Divider, Form, Input, Row, Select, Space, Statistic, Typography } from 'antd';
+import { DebounceSelect } from 'components/debounce_select';
+import { isEqual } from 'lodash';
+import moment from 'moment';
+import React, { useState } from 'react';
+import { useActionData } from 'react-router-dom';
+import { useAddress } from 'store/address/hook';
+import { FetchStatus } from 'type/FetchStatus';
+import { InformationCard } from './components/information_card';
+import { ICustomer } from 'type/customer';
 
+const data: any = {
+  "id": "10324",
+  "name": "Đức SiêuVương",
+  "email": "7kucSieu.Vuong@hotmail.com",
+  "extension": "+84",
+  "phone": "020 0006 1455",
+  "dob": moment("1944-03-05T00:00:00.000Z"),
+  "address": {
+    "id": "kTIUtaeRubxip01GlB4LzlqUQi34vZCLW7p3BIOOrR5bqkYz91aXyWq57XjqOVqXxbZN4MrlCpetqaV4PuKqH42FHc06gaIjAb21dSYt7i-Krbk4dvR2bi2y5RjaLHPDL",
+    "metadata": {
+      "url": "https://maps.goong.io/?pid=kTIUtaeRubxip01GlB4LzlqUQi34vZCLW7p3BIOOrR5bqkYz91aXyWq57XjqOVqXxbZN4MrlCpetqaV4PuKqH42FHc06gaIjAb21dSYt7i-Krbk4dvR2bi2y5RjaLHPDL",
+      "name": "Cửa Hàng Mobile Address",
+      "types": [
+        "grocery_store"
+      ],
+      "compound": {
+        "commune": "Tràng Tiền",
+        "district": "Hoàn Kiếm",
+        "province": "Hà Nội"
+      },
+      "geometry": {
+        "location": {
+          "lat": 21.023974229000032,
+          "lng": 105.85418242400004
+        }
+      },
+      "place_id": "kTIUtaeRubxip01GlB4LzlqUQi34vZCLW7p3BIOOrR5bqkYz91aXyWq57XjqOVqXxbZN4MrlCpetqaV4PuKqH42FHc06gaIjAb21dSYt7i-Krbk4dvR2bi2y5RjaLHPDL",
+      "plus_code": {
+        "global_code": "LNPP+RGBS",
+        "compound_code": "+RGBS Tràng Tiền, Hoàn Kiếm, Hà Nội"
+      },
+      "formatted_address": "Cửa Hàng Mobile Address, 16 Hai Bà Trưng, Tràng Tiền, Hoàn Kiếm, Hà Nội"
+    },
+    "createdAt": "2024-01-03T17:14:17.856Z",
+    "updatedAt": "2024-01-03T17:14:17.856Z"
+  }
+}
 
 export const DetailCustomer = () => {
-  return <h1>Customer detail</h1>
+
+  const address = useAddress();
+  const [form] = Form.useForm();
+  const [change, setChange] = useState(false);
+
+  const prefixSelector = (
+    <Form.Item name="extension" noStyle initialValue={"+84"}>
+      <Select style={{ width: 100 }}>
+        <Select.Option value="+84">🇻🇳 +84 </Select.Option>
+      </Select>
+    </Form.Item>
+  );
+
+  return <Row justify="space-around">
+    <Col span={8}>
+      <InformationCard data={data} />
+    </Col>
+    <Col span={14} />
+  </Row>
 }
