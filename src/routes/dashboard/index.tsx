2fd01e7136d9783a3458bd5ebec83fac5b4bd420
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-09 22:48:40 +0700

    Update dashboard

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -11,9 +11,9 @@ const Page = () => {
 
     var METABASE_SITE_URL = "http://metabase.relationc.com";
 
-    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE3MDQ3MjE1NjA0ODMwfQ.xUpO29yNhTB9pI_a8hHnRXZFDZmzJT6Qginu7imgaOY";
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE3MDQ4MDk4OTcwfQ.T8LOAa_kow3nUm0bCVdnZ-K6RsIN5NLmaqJ2u3lGJ9E";
 
-    return METABASE_SITE_URL + "/embed/dashboard/" + token + "#theme=null&bordered=false&titled=false";
+    return METABASE_SITE_URL + "/embed/dashboard/" + token + "#theme=transparent&bordered=false&titled=false";
 
   }
 
