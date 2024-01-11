9d64147fcd28b39595319c9ad32489dc2cb341a1
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-11 22:53:05 +0700

    Update header

--- src/routes/tag/index.tsx
+++ src/routes/tag/index.tsx
@@ -1,11 +1,21 @@
-import { Divider } from "antd";
+import { Divider, theme } from "antd";
 import React from "react";
 import { TagAction } from "./components/action";
 import { TagSpace } from "./components/tags";
+import { Constants } from "common/constant";
 
 const Page = () => {
+  const { token } = theme.useToken();
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{
+      maxWidth: Constants.MAX_WIDTH_CONTAINER,
+      marginLeft: "auto",
+      marginRight: "auto",
+      backgroundColor: token.colorBgContainer,
+      padding: token.padding,
+      borderRadius: token.borderRadiusLG,
+      boxShadow: token.boxShadow,
+    }}>
       <TagAction />
       <Divider />
       <TagSpace />
